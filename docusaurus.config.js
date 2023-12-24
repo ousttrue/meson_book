2dba3426609524c6eeff4e58407f7a3b47b334d8
ousttrue <ousttrue@gmail.com>
2023-12-25 02:58:33 +0900

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   tagline: 'meson buildtool tips',
 
   // Set the production url of your site here
-  url: 'ousttrue.github.io',
+  url: 'https://ousttrue.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/meson_book/',
